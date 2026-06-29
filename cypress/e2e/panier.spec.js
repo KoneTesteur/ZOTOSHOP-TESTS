6de3dec2ba0b62304c8ff504f6dffe3a4c620eb1
koneTesteur <12345678+KoneTesteur@users.noreply.github.com>
2026-06-29 21:41:39 +0200

    Ajouter de  Spec sur Panier

---
+++ cypress/e2e/panier.spec.js
@@ -0,0 +1,19 @@
+describe('Panier ZotoShop', () => {
+    beforeEach(() => {
+        cy.visit("/");
+    });
+    it('should add product to cart', () => {
+        cy.log("T000: implementer ce test");
+        
+    });
+    it('should update product quantity in cart', () => {
+        cy.log("T000: implementer ce test");
+        
+    });
+    it('should renove product from cart', () => {
+        cy.log("T000: implementer ce test");
+        cy.log("T000: implementer ce test2");
+        
+    });
+    
+});
