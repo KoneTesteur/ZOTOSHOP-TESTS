6caea4b55259e36afb9f130e819cbce87cae9e4d
koneTesteur <12345678+KoneTesteur@users.noreply.github.com>
2026-06-29 20:55:32 +0200

    Ajouter du Nouveau Fonctionnalité

---
+++ cypress/e2e/login.spec.js
@@ -0,0 +1,23 @@
+describe('Login ZotoShop', () => {
+beforeEach(() => {
+    cy.visit('/account/login');
+})
+
+it('should display login form', () => {
+    cy.log("T000: implementer ce Test");
+});
+
+it('should login with valid credentials', () => {
+    cy.log("T000: implementer ce Test");
+    cy.log("T000: implementer ce Test");
+});
+it('shoult rejet invalid credentials', () => {
+    cy.log("T000:implementer ce Test")
+});
+
+it( 'should reject invalid credentials', () => {
+    cy.log("T000: implementer ce Test1");
+    cy.log("T000: implementer ce Test2");
+    cy.log("T000: implementer ce Test3");
+})
+});
